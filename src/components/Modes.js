96108c00cbe61339023f28c5bf75de73fa775f43
Jctech15 <jyctech15@gmail.com>
2023-02-02 13:17:50 -0800

    theme changing logic added

--- src/components/Modes.js
+++ src/components/Modes.js
@@ -1,11 +1,11 @@
 import TimedMode from "./TimedMode";
 import Theme from "./Theme";
 
-export default function Modes({ gameStart }) {
+export default function Modes({ gameStart, handleLoseGame }) {
   return (
     <div id="Modes">
       <h1>Game Modes</h1>
-      <TimedMode gameStart={gameStart} />
+      <TimedMode gameStart={gameStart} handleLoseGame={handleLoseGame} />
       {gameStart ? null : <Theme />}
     </div>
   );
--- src/components/TimedMode.js
+++ src/components/TimedMode.js
@@ -1,30 +1,32 @@
 import { useEffect, useState } from "react";
 import Timer from "./Timer";
 
-export default function TimedMode({ gameStart }) {
+export default function TimedMode({ gameStart, handleLoseGame }) {
   const [timer, setTimer] = useState(null);
-
-  useEffect(() => {}, [gameStart]);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     if (gameStart) {
+      if (timer === 0) {
+        handleLoseGame();
+        setTimer(seconds);
+      }
       let ticker = setInterval(() => tick(), 1000);
       return () => {
         clearInterval(ticker);
       };
+    } else {
+      setTimer(seconds);
     }
-  });
+  }, [gameStart, tick, timer]);
 
   function onChangeValue(e) {
     setTimer(e.target.value);
+    setSeconds(e.target.value);
   }
 
   function tick() {
-    if (timer === 0) {
-      setTimer(0);
-    } else {
-      setTimer((seconds) => seconds - 1);
-    }
+    setTimer((seconds) => seconds - 1);
   }
 
   return (
